b16c7f9a1a201df3db3c5fda48bf0f5e4621c209
SenseiSamy <samy.naji2003@gmail.com>
2024-08-28 17:20:53 +0200

    add a send api to send message

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.MessageList.as_view()),
 	path('<int:pk>/', views.MessageDetail.as_view()),
-	path('<user>/', views.GetChat)
+	path('get/<user>/', views.GetChat),
+	path('send/', views.SendMessage)
 ]
--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,11 +1,12 @@
 from chat.models import Message
 from django.contrib.auth.models import User
 from chat.serializers import MessageSerializer
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 class MessageList(generics.ListCreateAPIView):
 	queryset = Message.objects.all()
@@ -24,3 +25,15 @@ def GetChat(request, user):
     chat = Message.objects.filter(sender=logged_user, receiver=other_user) | Message.objects.filter(sender=other_user, receiver=logged_user)
     serializer = MessageSerializer(chat, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def SendMessage(request):
+	sender = request.user
+	receiver = get_object_or_404(User, username=request.data['username'])
+	if (request.data['message'] != ""):
+		message = Message(sender=sender, receiver=receiver, content=request.data['message'])
+		message.save()
+		return Response(status=status.HTTP_201_CREATED)
+	return Response(status=status.HTTP_400_BAD_REQUEST)
