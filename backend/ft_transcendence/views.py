3870d8065715e9e3c91d340df9fd4ceae7dff568
SenseiSamy <samy.naji2003@gmail.com>
2024-08-29 17:59:58 +0200

    reworked login and register views

--- backend/ft_transcendence/views.py
+++ backend/ft_transcendence/views.py
@@ -4,35 +4,36 @@ from .serializers import UserSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.shortcuts import get_object_or_404
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+
 @api_view(['POST'])
 def login(request):
-	user = get_object_or_404(User, username=request.data['username'])
-	if not user.check_password(request.data['password']):
-		return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
+	user = authenticate(username=request.data.get('username'), password=request.data.get('password'))
+	if not user:
+		return Response({'success': False, 'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
 	token, created = Token.objects.get_or_create(user=user)
-	serializer = UserSerializer(instance=user)
-	return Response({"token": token.key, "user": serializer.data})
+	return Response({'success': True, 'token': token.key, 'username': user.username})
 
 @api_view(['POST'])
 def register(request):
 	serializer = UserSerializer(data=request.data)
-	if serializer.is_valid():
-		serializer.save()
-		user = User.objects.get(username=request.data['username'])
-		user.set_password(request.data['password'])
-		user.save()
-		token = Token.objects.create(user=user)
-		return Response({"token": token.key, "user": serializer.data})
-	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	if not serializer.is_valid():
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	serializer.save()
+	user = User.objects.get(username=request.data['username'])
+	user.set_password(request.data['password'])
+	user.save()
+	token = Token.objects.create(user=user)
+	return Response({'success': True, 'token': token.key, 'user': user.username})
 
 @api_view(['POST'])
-@authentication_classes(TokenAuthentication)
-@permission_classes(IsAuthenticated)
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def logout(request):
 	token = Token.objects.get(user=request.user)
 	token.delete()
-	return(Response({"success": True, "detail": "Logged out!"}, status=status.HTTP_200_OK))
+	return(Response({'success': True, 'detail': 'Logged out!'}, status=status.HTTP_200_OK))
