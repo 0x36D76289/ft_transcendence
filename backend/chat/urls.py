d40e8d351310c2066ebbdd54aa434326eb5704ce
SenseiSamy <samy.naji2003@gmail.com>
2024-08-29 20:13:34 +0200

    added game history endpoint + clean project

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.MessageList.as_view()),
-	path('<int:pk>/', views.MessageDetail.as_view()),
-	path('get/<user>/', views.GetChat),
-	path('send/', views.SendMessage)
+	path('<int:pk>', views.MessageDetail.as_view()),
+	path('get/<user>', views.GetChat),
+	path('send', views.SendMessage)
 ]
--- backend/game/urls.py
+++ backend/game/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('history', views.GameAllHistory),
+	path('history/<user>', views.GameUserHistory),
 	path('', views.GameList.as_view()),
-	path('<int:pk>/', views.GameDetail.as_view())
+	path('<int:pk>', views.GameDetail.as_view())
 ]
