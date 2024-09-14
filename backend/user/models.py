1b9c04174f34cde12ee38fab2adc87e863d275e2
SenseiSamy <samy.naji2003@gmail.com>
2024-09-14 02:06:12 +0200

    add user stats

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-from user.manager import UserManager
 
 class User(AbstractBaseUser):
 	username = models.CharField(max_length=20, unique=True)
