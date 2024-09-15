3370ca4b52f48bccaf7f6cca5cdf0b7f30cfcdfb
wmemcpy <celeste.francier@protonmail.com>
2024-09-15 18:37:52 +0200

    Refactor app.js to remove unused code and update routes

--- frontend/static-html/js/app.js
+++ frontend/static-html/js/app.js
@@ -2,13 +2,11 @@ import { start } from "./spa.js";
 import { addRoute } from "./spa.js";
 
 import { renderHome } from "./pages/home.js";
-import { renderPlay } from "./pages/play.js";
 import { renderAccount } from "./pages/account.js";
 
 // entry point
 document.addEventListener("DOMContentLoaded", () => {
   addRoute("/", renderHome);
-  addRoute("/play", renderPlay);
   addRoute("/account", renderAccount);
 
   start();
