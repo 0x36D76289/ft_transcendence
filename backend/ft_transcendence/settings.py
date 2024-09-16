3217a7fac395f0ae89724dea8bb640a74e39985f
SenseiSamy <samy.naji2003@gmail.com>
2024-09-16 16:14:47 +0200

    added cors to the backend api

--- backend/ft_transcendence/settings.py
+++ backend/ft_transcendence/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 	'rest_framework',
 	'rest_framework.authtoken',
+	'corsheaders',
 	'user',
 	'game',
 	'chat'
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+	'https://localhost:8443'
+]
