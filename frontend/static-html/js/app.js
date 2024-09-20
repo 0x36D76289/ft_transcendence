ac36d5db4c30b75af85e2db7dc686d34a054ddb0
0x36D76289 <71098898+0x36D76289@users.noreply.github.com>
2024-09-21 00:15:47 +0200

    Refactor createSelect function to dynamically create select options and add checkAuthStatus function to app.js Add getCookie utility function to cookie.js

--- frontend/static-html/js/app.js
+++ frontend/static-html/js/app.js
@@ -1,4 +1,4 @@
-import { start, addRoute } from "./spa.js";
+import { start, addRoute, checkAuthStatus } from "./spa.js";
 import { renderHome } from "./pages/home.js";
 import { renderProfile } from "./pages/account/profile.js";
 import { renderHub } from "./pages/hub.js";
@@ -8,13 +8,19 @@ import { renderConnexion } from "./pages/account/connexion.js";
 export let currentUser = null;
 
 // entry point
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  // Vérification de l'état de connexion de l'utilisateur
+  try {
+    currentUser = await checkAuthStatus(); // Renvoie null si non connecté
+  } catch (error) {
+    console.error("Erreur de vérification de l'authentification", error);
+  }
+
+  // Routes
   addRoute("/", renderHome);
   addRoute("/hub", renderHub);
-
   addRoute("/profile", renderProfile);
   addRoute("/connexion", renderConnexion);
-
   addRoute("/pacman", renderPacman);
 
   start();
