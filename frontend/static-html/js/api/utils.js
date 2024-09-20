c99b70e8616ec8d981c03307eeb86e4e93125a33
wmemcpy <celeste.francier@protonmail.com>
2024-09-21 00:14:43 +0200

    Refactor API request functions to include CSRF token

--- frontend/static-html/js/api/utils.js
+++ frontend/static-html/js/api/utils.js
@@ -1,51 +1,53 @@
 import { logMessage } from "../logs.js";
+import { getCookie } from "../cookie.js";
 
 const API_BASE_URL = "http://127.0.0.1:8000";
 
 /**
- * Sends a POST request to the specified URL with the given data and optional token.
+ * Sends a POST request to the specified URL with the given data and optional CSRF token.
  *
  * @param {string} url - The endpoint URL to send the POST request to.
  * @param {Object} data - The data to be sent in the body of the POST request.
- * @param {string|null} [token=null] - Optional authorization token to be included in the request headers.
  * @returns {Promise<Object>} - A promise that resolves to the JSON response from the server.
  */
-export const postData = async (url = "", data = {}, token = null) => {
+export const postData = async (url = "", data = {}) => {
 	const headers = {
 		"Content-Type": "application/json",
+		"X-CSRFToken": getCookie("csrftoken"), // Protection CSRF
 	};
-	if (token) {
-		headers["Authorization"] = `Token ${token}`;
-	}
 
 	const response = await fetch(`${API_BASE_URL}${url}`, {
 		method: "POST",
 		headers: headers,
 		body: JSON.stringify(data),
+		credentials: "include",
 	});
 
 	logMessage(`POST ${url} ${response.status}`, "info");
+
+	if (!response.ok) {
+		logMessage(`POST ${url} failed with status ${response.status}`, "error");
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
 
 	return response.json();
 };
 
 /**
- * Fetches data from the specified URL with optional data and token.
+ * Fetches data from the specified URL with optional data and CSRF token.
  *
  * @param {string} url - The endpoint URL to fetch data from.
- * @param {Object} [data={}] - The data to be sent with the request.
- * @param {string|null} [token=null] - The optional authorization token.
  * @returns {Promise<Object>} - A promise that resolves to the response data in JSON format.
  */
-export const getData = async (url = "", token = null) => {
-	const headers = {};
-	if (token) {
-		headers["Authorization"] = `Token ${token}`;
-	}
+export const getData = async (url = "") => {
+	const headers = {
+		"X-CSRFToken": getCookie("csrftoken"),
+	};
 
 	const response = await fetch(`${API_BASE_URL}${url}`, {
 		method: "GET",
 		headers: headers,
+		credentials: "include",
 	});
 
 	if (!response.ok) {
