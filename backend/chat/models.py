c6c34010633dc1b8f838407d65f86af5c0931c02
SenseiSamy <samy.naji2003@gmail.com>
2024-10-02 20:52:18 +0200

    backend: add live chat with channels

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from user.models import User
 
+class Conversation(models.Model):
+	initiator = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='conv_initiator', null=True)
+	receiver = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='conv_participant', null=True)
+	start_time = models.DateTimeField(auto_now_add=True)
+
 class Message(models.Model):
-	sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
-	receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+	sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_sender')
 	content = models.TextField(max_length=300)
+	conversation_id = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='message_set')
 	time_created = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		ordering = ('-time_created',)
 
 class UserBlock(models.Model):
 	uid1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_block_uid1');
--- backend/chat/serializers.py
+++ backend/chat/serializers.py
@@ -1,11 +1,35 @@
 from rest_framework import serializers
-from chat.models import Message
+from chat.models import Message, Conversation
 from user.serializers import UserSerializer
 
 class MessageSerializer(serializers.ModelSerializer):
-	sender = UserSerializer()
-	receiver = UserSerializer()
+	sender = serializers.SlugRelatedField(
+		read_only=True,
+		slug_field='username'
+	)
 
 	class Meta:
 		model = Message
-		fields = ['sender', 'receiver', 'content', 'time_created']
+		exclude = ('conversation_id',)
+
+class ConversationListSerializer(serializers.ModelSerializer):
+	initiator = UserSerializer()
+	receiver = UserSerializer()
+	last_message = serializers.SerializerMethodField()
+
+	class Meta:
+		model = Conversation
+		fields = ['id', 'initiator', 'receiver', 'last_message']
+
+	def get_last_message(self, instance):
+		message = instance.message_set.first()
+		return MessageSerializer(instance=message).data
+	
+class ConversationSerializer(serializers.ModelSerializer):
+	initiator = UserSerializer()
+	receiver = UserSerializer()
+	message_set = MessageSerializer(many=True)
+
+	class Meta:
+		model = Conversation
+		fields = ['id', 'initiator', 'receiver', 'message_set']
