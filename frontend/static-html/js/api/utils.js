ab421d1d97ae415419c1303c09e6dd7ace313b7b
wmemcpy <celeste.francier@protonmail.com>
2024-10-07 03:50:41 +0200

    Refactor utils.js: Add cURL command logging and improve error handling

--- frontend/static-html/js/api/utils.js
+++ frontend/static-html/js/api/utils.js
@@ -1,36 +1,49 @@
 const API_BASE_URL = "http://127.0.0.1:8000";
 
-export async function postData(url = "", headers = {}, body = {}) {
-	headers["Content-Type"] = "application/json";
+function logCurlCommand(url, method, headers, body) {
+	let curl = `curl -X ${method} "${API_BASE_URL}${url}"`;
+	for (const [key, value] of Object.entries(headers)) {
+		curl += ` -H "${key}: ${value}"`;
+	}
+	if (body) {
+		curl += ` -d '${JSON.stringify(body)}'`;
+	}
+	console.log(curl);
+}
 
-	console.log(`curl -X POST -H "${JSON.stringify(headers)}" -d '${JSON.stringify(body)}' ${API_BASE_URL}${url}`);
+export async function postData(url = "", headers = {}, body = {}) {
+	logCurlCommand(url, "POST", headers, body);
 
 	const response = await fetch(`${API_BASE_URL}${url}`, {
 		method: "POST",
-		headers: headers,
-		body: JSON.stringify(body),
+		headers: {
+			"Content-Type": "application/json",
+			...headers
+		},
+		body: JSON.stringify(body)
 	});
 
-	const responseData = await response.json();
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
 
-	console.log(responseData);
-
-	return responseData;
+	return response.json();
 }
 
 export async function getData(url = "", headers = {}) {
-	headers["Content-Type"] = "application/json";
-
-	console.log(`curl -X GET -H "${JSON.stringify(headers)}" ${API_BASE_URL}${url}`);
+	logCurlCommand(url, "GET", headers);
 
 	const response = await fetch(`${API_BASE_URL}${url}`, {
 		method: "GET",
-		headers: headers,
+		headers: {
+			"Content-Type": "application/json",
+			...headers
+		}
 	});
 
-	const responseData = await response.json();
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
 
-	console.log(responseData);
-
-	return responseData;
+	return response.json();
 }
