3dcea3461074b81032d6b9dce9f4ba126cc080b7
SenseiSamy <samy.naji2003@gmail.com>
2024-10-08 17:52:18 +0200

    changed backend to frontend nginx reverse proxy, now request to backend are made to https://localhost:8443/api/

--- backend/ft_transcendence/tokenauth_middleware.py
+++ backend/ft_transcendence/tokenauth_middleware.py
@@ -3,22 +3,25 @@ from channels.db import database_sync_to_async
 from rest_framework.authtoken.models import Token
 from channels.middleware import BaseMiddleware
 
+import sys
+
 @database_sync_to_async
 def get_user(token_key):
-    try:
-        token = Token.objects.get(key=token_key)
-        return token.user
-    except Token.DoesNotExist:
-        return AnonymousUser()
+	try:
+		token = Token.objects.get(key=token_key)
+		return token.user
+	except Token.DoesNotExist:
+		return AnonymousUser()
 
 class TokenAuthMiddleware(BaseMiddleware):
-    def __init__(self, inner):
-        self.inner = inner
+	def __init__(self, inner):
+		self.inner = inner
 
-    async def __call__(self, scope, receive, send):
-        headers = dict(scope['headers'])
-        if b'authorization' in headers:
-            token_name, token_key = headers[b'authorization'].decode().split()
-            if token_name == 'Token':
-                scope['user'] = await get_user(token_key)
-        return await super().__call__(scope, receive, send)
+	async def __call__(self, scope, receive, send):
+		query_string = scope['query_string'].split(b'&')
+		print(f"QUERY_STRING: {query_string}", file=sys.stderr)
+		for s in query_string:
+			if s.startswith(b'token='):
+				s = s.decode()
+				scope['user'] = await get_user(s[6:])
+		return await super().__call__(scope, receive, send)
