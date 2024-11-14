c925fa629642c50da08925aa89235d09b256194c
wmemcpy <celeste.francier@protonmail.com>
2024-11-14 04:51:20 +0100

    style: update WebSocket base URL and refine chat API endpoint handling

--- frontend/static-html/js/api/chat.js
+++ frontend/static-html/js/api/chat.js
@@ -3,9 +3,9 @@ import { getToken } from '../utils/cookies.js';
 
 export const ChatAPI = {
 	getConversation: (convoId) => get(`/chat/${convoId}`, getToken()),
-	getConversations: () => get('/chat', getToken()),
+	getConversations: () => get('/chat/conversations', getToken()),
 	startConversation: (username) => post('/chat/start', { username }, getToken()),
 	blockUser: (username) => post('/chat/block', { username }, getToken()),
 	unblockUser: (username) => post('/chat/unblock', { username }, getToken()),
-	isUserBlocked: (username) => get(`/chat/is_user_blocked`, getToken()),
+	isUserBlocked: (username) => get(`/chat/is_user_blocked`, { username }, getToken()),
 };
