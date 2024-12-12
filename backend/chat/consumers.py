9432ca7059f3670da57df6f480998eb592f6f352
milan aduaka <milan.aduaka@pm.me>
2024-12-12 20:15:59 +0100

    added prints and a repr for conversation

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -13,68 +13,67 @@ from chat.serializers import MessageSerializer
 
 from sys import stderr
 
+
 class ChatConsumer(WebsocketConsumer):
-	def connect(self):
-		self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-		self.room_group_name = f"chat_{self.room_name}"
-		sender = self.scope.get("user")
+    def connect(self):
+        print("CONNECTING TO SOCKET", file=stderr)
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+        sender = self.scope.get("user")
 
-		try:
-			print("on est la", file = stderr)
-			conversation = Conversation.objects.get(id=int(self.room_name))
-			sender = self.scope["user"]
-			print(sender.username, file = stderr)
-			print(conversation.participants.all(), file = stderr)
-			if sender not in conversation.participants.all():
-				print("user not in conv", file = stderr)
-				raise
-		except:
-			self.close()
-			return
+        try:
+            print("on est la", file=stderr)
+            print([obj for obj in Conversation.objects.all()], file=stderr)
+            conversation = Conversation.objects.get(id=int(self.room_name))
+            print("on est la 2", file=stderr)
+            sender = self.scope["user"]
+            print(sender.username, file=stderr)
+            print(conversation.participants.all(), file=stderr)
+            if sender not in conversation.participants.all():
+                print("user not in conv", file=stderr)
+                raise Exception()
+        except Exception as e:
+            print("found error", e, file=stderr)
+            self.close()
+            return
 
-		# Join room group
-		async_to_sync(self.channel_layer.group_add)(
-			self.room_group_name, self.channel_name
-		)
-		self.accept()
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+        self.accept()
 
-	def disconnect(self, close_code):
-		# Leave room group
-		async_to_sync(self.channel_layer.group_discard)(
-			self.room_group_name, self.channel_name
-		)
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
-	# Receive message from WebSocket
-	def receive(self, text_data=None, bytes_data=None):
-		# parse the json data into dictionary object
-		text_data_json = json.loads(text_data)
-		message = text_data_json["message"]
+    # Receive message from WebSocket
+    def receive(self, text_data=None, bytes_data=None):
+        # parse the json data into dictionary object
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
 
-		conversation = Conversation.objects.get(id=int(self.room_name))
-		sender = self.scope["user"]
+        conversation = Conversation.objects.get(id=int(self.room_name))
+        sender = self.scope["user"]
 
-		_message = Message.objects.create(
-			sender=sender,
-			content=message,
-			conversation_id=conversation
-		)
-		# Send message to room group
-		chat_type = {"type": "chat_message"}
-		message_serializer = (dict(MessageSerializer(instance=_message).data))
-		return_dict = {**chat_type, **message_serializer}
-		async_to_sync(self.channel_layer.group_send)(
-			self.room_group_name,
-			return_dict,
-		)
+        _message = Message.objects.create(
+            sender=sender, content=message, conversation_id=conversation
+        )
+        # Send message to room group
+        chat_type = {"type": "chat_message"}
+        message_serializer = dict(MessageSerializer(instance=_message).data)
+        return_dict = {**chat_type, **message_serializer}
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            return_dict,
+        )
 
-	# Receive message from room group
-	def chat_message(self, event):
-		dict_to_be_sent = event.copy()
-		dict_to_be_sent.pop("type")
+    # Receive message from room group
+    def chat_message(self, event):
+        dict_to_be_sent = event.copy()
+        dict_to_be_sent.pop("type")
 
-		# Send message to WebSocket
-		self.send(
-				text_data=json.dumps(
-					dict_to_be_sent
-				)
-			)
+        # Send message to WebSocket
+        self.send(text_data=json.dumps(dict_to_be_sent))
--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,19 +1,33 @@
 from django.db import models
 from user.models import User
 
+
 class Conversation(models.Model):
-	participants = models.ManyToManyField(User)
-	start_time = models.DateTimeField(auto_now_add=True)
+    participants = models.ManyToManyField(User)
+    start_time = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self) -> str:
+        return f"<Conversation {self.id}>"
+
 
 class Message(models.Model):
-	sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_sender')
-	content = models.TextField(max_length=300)
-	conversation_id = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='message_set')
-	time_created = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="message_sender"
+    )
+    content = models.TextField(max_length=300)
+    conversation_id = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="message_set"
+    )
+    time_created = models.DateTimeField(auto_now_add=True)
 
-	class Meta:
-		ordering = ('-time_created',)
+    class Meta:
+        ordering = ("-time_created",)
+
 
 class UserBlock(models.Model):
-	uid1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_block_uid1');
-	uid2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_block_uid2');
+    uid1 = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_block_uid1"
+    )
+    uid2 = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_block_uid2"
+    )
