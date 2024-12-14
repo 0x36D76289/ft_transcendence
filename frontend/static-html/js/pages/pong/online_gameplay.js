8eb298cd1ed9c1e265b81c2b2aa3f37bf1634b36
Milhax <hack3900@milhax.com>
2024-12-14 04:40:35 +0100

    improved collisions, next up: improving the netcode to reduce teleportations

--- frontend/static-html/js/pages/pong/online_gameplay.js
+++ frontend/static-html/js/pages/pong/online_gameplay.js
@@ -3,7 +3,18 @@
 import { copy_gamestate } from "./gamestate.js";
 import { keys } from "./input.js";
 import { draw } from "./render.js";
-import { ball_bounce_y_speed, current_state, init, inputs, interval, is_ball_bouncing, is_ball_on_left, is_ball_on_wall, scores, tick_bounce_vertical, tick_move_ball, tick_move_paddle } from "./shared_gameplay.js";
+import {
+  current_state,
+  detect_death,
+  init,
+  inputs,
+  interval,
+  scores,
+  tick_bounce_horizontal,
+  tick_bounce_vertical,
+  tick_move_ball,
+  tick_move_paddle,
+} from "./shared_gameplay.js";
 import { game_sock } from "./socket.js";
 import { vec2 } from "./types.js";
 
@@ -15,12 +26,12 @@ import { vec2 } from "./types.js";
 /** @type {number} */
 export var player_num = 0;
 
-/** 
-	* @param {number} n 
-	* @returns {void}
-*/
+/**
+ * @param {number} n
+ * @returns {void}
+ */
 export function set_player_num(n) {
-	player_num = n;
+  player_num = n;
 }
 
 /** @type {number} */
@@ -32,46 +43,69 @@ var current_frame = 0;
 /** @type {import("./types").GameState} */
 var last_processed_state = Object.assign({}, current_state);
 
+// /**
+//  * @param {GameState} state
+//  * @param {vec2} input
+//  * @returns {void}
+//  * INFO: MODIFIES STATE
+//  */
+// function multiplayer_tick_bounce_horizontal(state, input) {
+//   if (!is_ball_on_wall(state)) {
+//     return;
+//   }
+//   state.ball_speed_x *= -1;
+//   if (is_ball_on_left(state)) {
+//     if (player_num == 1 && !is_ball_bouncing(state.ball_y, state.p1_height)) {
+//       scores[1] += 1;
+//       game_sock.send(
+//         JSON.stringify({
+//           type: "score",
+//           p1: scores[0],
+//           p2: scores[1],
+//         }),
+//       );
+//       if (interval) clearInterval(interval);
+//     } else {
+//       state.ball_speed_y = ball_bounce_y_speed(state.ball_y, state.p1_height);
+//     }
+//   } else {
+//     if (player_num == 2 && !is_ball_bouncing(state.ball_y, state.p2_height)) {
+//       scores[0] += 1;
+//       game_sock.send(
+//         JSON.stringify({
+//           type: "score",
+//           p1: scores[0],
+//           p2: scores[1],
+//         }),
+//       );
+//       if (interval) clearInterval(interval);
+//     } else {
+//       state.ball_speed_y = ball_bounce_y_speed(state.ball_y, state.p2_height);
+//     }
+//   }
+// }
+
 /**
-	* @param {GameState} state
-	* @param {vec2} input
-	* @returns {void}
-	* INFO: MODIFIES STATE 
-*/
-function multiplayer_tick_bounce_horizontal(state, input) {
-	//TODO: IMPROVE COLLISION
-	//		SET X SPEED TO 0 IF ENEMY NO BOUNCE
-	if (!is_ball_on_wall(state)) {
-		return;
-	}
-	state.ball_speed_x *= -1
-	if (is_ball_on_left(state)) {
-		if (player_num == 1 && !is_ball_bouncing(state.ball_y, state.p1_height)) {
-			scores[1] += 1;
-			game_sock.send(JSON.stringify({
-				type: "score",
-				p1: scores[0],
-				p2: scores[1]
-			}));
-			if (interval)
-				clearInterval(interval);
-		} else {
-			state.ball_speed_y = ball_bounce_y_speed(state.ball_y, state.p1_height);
-		}
-	} else {
-		if (player_num == 2 && !is_ball_bouncing(state.ball_y, state.p2_height)) {
-			scores[0] += 1;
-			game_sock.send(JSON.stringify({
-				type: "score",
-				p1: scores[0],
-				p2: scores[1]
-			}));
-			if (interval)
-				clearInterval(interval);
-		} else {
-			state.ball_speed_y = ball_bounce_y_speed(state.ball_y, state.p2_height);
-		}
-	}
+ * @param {GameState} state
+ * @returns {void}
+ * INFO: MODIFIES STATE
+ */
+function tick_multiplayer_death_processing(state) {
+  /** @type {number} */
+  let death = detect_death(state);
+
+  if (death) state.ball_speed_x = 0;
+  if (death && player_num == death) {
+    scores[death % 2] += 1;
+    if (interval) clearInterval(interval);
+    game_sock.send(
+      JSON.stringify({
+        type: "score",
+        p1: scores[0],
+        p2: scores[1],
+      }),
+    );
+  }
 }
 
 /**
@@ -81,86 +115,92 @@ function multiplayer_tick_bounce_horizontal(state, input) {
 	//INFO: takes a state and an input, simulates the next state and returns it
 */
 function multiplayer_tick(state, input) {
-	let ret = Object.assign({}, state);
+  let ret = Object.assign({}, state);
 
-	ret.frame = state.frame + 1;
+  ret.frame = state.frame + 1;
 
-	tick_move_paddle(ret, input);
-	tick_move_ball(ret, input);
-	tick_bounce_vertical(ret, input);
-	multiplayer_tick_bounce_horizontal(ret, input);
+  tick_move_paddle(ret, input);
+  tick_move_ball(ret, input);
+  tick_bounce_vertical(ret, input);
+  tick_bounce_horizontal(ret, input);
+  //shared bounce method
+  // multiplayer_tick_bounce_horizontal(ret, input);
+  tick_multiplayer_death_processing(ret);
 
-	return ret;
+  return ret;
 }
 
 /**
-	* @returns {void}
-*/
+ * @returns {void}
+ */
 export function multiplayer_update() {
-	// go forward
-	current_frame += 1;
+  // go forward
+  current_frame += 1;
 
-	// process local input
-	if (inputs.length <= current_frame) {
-		inputs[current_frame] = [undefined, undefined];
-	}
-	if (keys.has("ArrowUp") || keys.has("KeyW")) {
-		inputs[current_frame][player_num - 1] = -1;
-	} else if (keys.has("ArrowDown") || keys.has("KeyS")) {
-		inputs[current_frame][player_num - 1] = 1;
-	} else {
-		inputs[current_frame][player_num - 1] = 0;
-	}
+  // process local input
+  if (inputs.length <= current_frame) {
+    inputs[current_frame] = [undefined, undefined];
+  }
+  if (keys.has("ArrowUp") || keys.has("KeyW")) {
+    inputs[current_frame][player_num - 1] = -1;
+  } else if (keys.has("ArrowDown") || keys.has("KeyS")) {
+    inputs[current_frame][player_num - 1] = 1;
+  } else {
+    inputs[current_frame][player_num - 1] = 0;
+  }
 
-	//send input
-	game_sock.send(JSON.stringify({
-		type: "input",
-		frame: current_frame,
-		input: inputs[current_frame][player_num - 1],
-		player: player_num,
-	}));
+  //send input
+  game_sock.send(
+    JSON.stringify({
+      type: "input",
+      frame: current_frame,
+      input: inputs[current_frame][player_num - 1],
+      player: player_num,
+    }),
+  );
 
-	// zip to last processed frame and recalculate until now
-	// assume input is same for every frame since last input received before the calculated instance
-	// looks incorrect
-	let validating = true;
-	let frame_input_count = 0;
-	copy_gamestate(last_processed_state, current_state);
-	let input = [ // TODO: check off by one (it works doe)
-		inputs[current_state.frame][0],
-		inputs[current_state.frame][1]
-	];
-	while (current_state.frame < current_frame) {
-		frame_input_count = 0;
-		copy_gamestate(multiplayer_tick(current_state, input), current_state);
-		if (inputs[current_state.frame][0] != undefined) {
-			input[0] = inputs[current_state.frame][0];
-			frame_input_count += 1;
-		}
-		if (inputs[current_state.frame][1] != undefined) {
-			input[1] = inputs[current_state.frame][1];
-			frame_input_count += 1;
-		}
-		if (validating && frame_input_count == 2) {
-			last_processed_state = Object.assign({}, current_state);
-			last_processed_frame = last_processed_state.frame;
-		} else {
-			validating = false;
-		}
-	}
-	draw(current_state, scores);
-	// create array of size 3
-	// if array less than size 3 append to array
-	// if array size 3 shift left and set input to pos 3
-	// render position 1?
-	//TODO: save 3 frame buffer and render a bit late
-	// create an array of size 3 and save during while loop at the right place
-	// only draw if array is of size 3
+  // zip to last processed frame and recalculate until now
+  // assume input is same for every frame since last input received before the calculated instance
+  // looks incorrect
+  let validating = true;
+  let frame_input_count = 0;
+  copy_gamestate(last_processed_state, current_state);
+  let input = [
+    // TODO: check off by one (it works doe)
+    inputs[current_state.frame][0],
+    inputs[current_state.frame][1],
+  ];
+  while (current_state.frame < current_frame) {
+    frame_input_count = 0;
+    copy_gamestate(multiplayer_tick(current_state, input), current_state);
+    if (inputs[current_state.frame][0] != undefined) {
+      input[0] = inputs[current_state.frame][0];
+      frame_input_count += 1;
+    }
+    if (inputs[current_state.frame][1] != undefined) {
+      input[1] = inputs[current_state.frame][1];
+      frame_input_count += 1;
+    }
+    if (validating && frame_input_count == 2) {
+      last_processed_state = Object.assign({}, current_state);
+      last_processed_frame = last_processed_state.frame;
+    } else {
+      validating = false;
+    }
+  }
+  draw(current_state, scores);
+  // create array of size 3
+  // if array less than size 3 append to array
+  // if array size 3 shift left and set input to pos 3
+  // render position 1?
+  //TODO: save 3 frame buffer and render a bit late
+  // create an array of size 3 and save during while loop at the right place
+  // only draw if array is of size 3
 }
 
 export function multiplayer_init() {
-	init();
-	current_frame = 0;
-	last_processed_frame = 0;
-	last_processed_state = Object.assign({}, current_state);
+  init();
+  current_frame = 0;
+  last_processed_frame = 0;
+  last_processed_state = Object.assign({}, current_state);
 }
--- frontend/static-html/js/pages/pong/shared_gameplay.js
+++ frontend/static-html/js/pages/pong/shared_gameplay.js
@@ -3,7 +3,6 @@ import { GAME_DIMENSIONS } from "./globals.js";
 
 //@ts-check
 
-
 /**
  * @typedef {import("./types").GameState} GameState
  * @typedef {import("./types").input} input
@@ -11,36 +10,36 @@ import { GAME_DIMENSIONS } from "./globals.js";
 
 /** @type {GameState} */
 export const current_state = {
-	frame: 0,
-	ball_x: 0,
-	ball_y: 0,
-	p1_height: 0,
-	p2_height: 0,
-	ball_speed_x: 0,
-	ball_speed_y: 0,
-	countdown_frames: 0,
+  frame: 0,
+  ball_x: 0,
+  ball_y: 0,
+  p1_height: 0,
+  p2_height: 0,
+  ball_speed_x: 0,
+  ball_speed_y: 0,
+  countdown_frames: 0,
 };
 
 export const GAME_SETTINGS = {
-	paddle_width: 5,
-	paddle_height: 20,
-	paddle_speed: 4,
-	ball_size: 3,
-	ball_speed: 5,
-	ball_y_speed_factor: 3,
-	win_score: 10,
-	countdown_length: 180,
+  paddle_width: 5,
+  paddle_height: 20,
+  paddle_speed: 4,
+  ball_size: 3,
+  ball_speed: 5,
+  ball_y_speed_factor: 3,
+  win_score: 10,
+  countdown_length: 180,
 };
 
 /** @type {?ReturnType<typeof setInterval>} */
 export var interval = null;
 
 /**
-	* @param {Function(): void} func
-	* @returns {void}
-*/
+ * @param {Function(): void} func
+ * @returns {void}
+ */
 export function start_simulation(func) {
-	interval = setInterval(func, 1000 / 60); // 60 fps
+  interval = setInterval(func, 1000 / 60); // 60 fps
 }
 
 /** @type {Array<input | undefined>[]} */
@@ -53,125 +52,181 @@ export const bots = new vec2(0, 0);
 export var scores = new vec2(0, 0);
 
 /**
-	* sets all the values for the start of the round
-	* @returns {void}
-*/
+ * sets all the values for the start of the round
+ * @returns {void}
+ */
 export function init() {
-	inputs = [[0, 0]];
-	current_state.frame = 0;
-	current_state.ball_x = (GAME_DIMENSIONS[0] / 2) | 0;
-	current_state.ball_y = (GAME_DIMENSIONS[1] / 2) | 0;
-	current_state.p1_height = current_state.ball_y;
-	current_state.p2_height = current_state.ball_y;
-	current_state.ball_speed_y = -GAME_SETTINGS.ball_speed;
-	current_state.ball_speed_x = GAME_SETTINGS.ball_speed;
-	current_state.countdown_frames = GAME_SETTINGS.countdown_length;
-	if (scores[0] <= scores[1]) {
-		current_state.ball_speed_x *= -1;
-	}
+  inputs = [[0, 0]];
+  current_state.frame = 0;
+  current_state.ball_x = (GAME_DIMENSIONS[0] / 2) | 0;
+  current_state.ball_y = (GAME_DIMENSIONS[1] / 2) | 0;
+  current_state.p1_height = current_state.ball_y;
+  current_state.p2_height = current_state.ball_y;
+  current_state.ball_speed_y = -GAME_SETTINGS.ball_speed;
+  current_state.ball_speed_x = GAME_SETTINGS.ball_speed;
+  current_state.countdown_frames = GAME_SETTINGS.countdown_length;
+  if (scores[0] <= scores[1]) {
+    current_state.ball_speed_x *= -1;
+  }
 }
 
 /**
-	* returns the position pos such that it doesn't reach outside min/max
-	* @param {number} pos
-	* @param {number} size
-	* @param {number} min
-	* @param {number} max
-	* @returns {number}
-*/
+ * returns the position pos such that it doesn't reach outside min/max
+ * @param {number} pos
+ * @param {number} size
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
 function bound(pos, size, min, max) {
-	if (pos - size < min)
-		return min + size;
-	if (pos + size > max)
-		return max - size;
-	return pos;
+  if (pos - size < min) return min + size;
+  if (pos + size > max) return max - size;
+  return pos;
 }
 
 /**
-	* @param {GameState} state
-	* @param {vec2} input
-	* @returns {void}
-	* INFO: MODIFIES STATE 
-*/
+ * @param {GameState} state
+ * @param {vec2} input
+ * @returns {void}
+ * INFO: MODIFIES STATE
+ */
 export function tick_move_paddle(state, input) {
-	state.p1_height += input[0] * GAME_SETTINGS.paddle_speed;
-	state.p2_height += input[1] * GAME_SETTINGS.paddle_speed;
-	state.p1_height = bound(state.p1_height, GAME_SETTINGS.paddle_height, 0, GAME_DIMENSIONS[1]);
-	state.p2_height = bound(state.p2_height, GAME_SETTINGS.paddle_height, 0, GAME_DIMENSIONS[1]);
+  state.p1_height += input[0] * GAME_SETTINGS.paddle_speed;
+  state.p2_height += input[1] * GAME_SETTINGS.paddle_speed;
+  state.p1_height = bound(
+    state.p1_height,
+    GAME_SETTINGS.paddle_height,
+    0,
+    GAME_DIMENSIONS[1],
+  );
+  state.p2_height = bound(
+    state.p2_height,
+    GAME_SETTINGS.paddle_height,
+    0,
+    GAME_DIMENSIONS[1],
+  );
 }
 
 /**
-	* @param {GameState} state
-	* @param {vec2} input
-	* @returns {void}
-	* INFO: MODIFIES STATE 
-*/
+ * @param {GameState} state
+ * @param {vec2} input
+ * @returns {void}
+ * INFO: MODIFIES STATE
+ */
 export function tick_move_ball(state, input) {
-	if (state.countdown_frames > 0) {
-		state.countdown_frames -= 1;
-	} else {
-		state.ball_x += state.ball_speed_x;
-		state.ball_y += state.ball_speed_y;
-		state.ball_x = bound(state.ball_x, GAME_SETTINGS.ball_size, 0, GAME_DIMENSIONS[0]);
-		state.ball_y = bound(state.ball_y, GAME_SETTINGS.ball_size, 0, GAME_DIMENSIONS[1]);
-	}
+  if (state.countdown_frames > 0) {
+    state.countdown_frames -= 1;
+  } else {
+    state.ball_x += state.ball_speed_x;
+    state.ball_y += state.ball_speed_y;
+    state.ball_x = bound(
+      state.ball_x,
+      GAME_SETTINGS.ball_size,
+      0,
+      GAME_DIMENSIONS[0],
+    );
+    state.ball_y = bound(
+      state.ball_y,
+      GAME_SETTINGS.ball_size,
+      0,
+      GAME_DIMENSIONS[1],
+    );
+  }
 }
 
 /**
-	* @param {GameState} state
-	* @param {vec2} input
-	* @returns {void}
-	* INFO: MODIFIES STATE 
-*/
+ * @param {GameState} state
+ * @param {vec2} input
+ * @returns {void}
+ * INFO: MODIFIES STATE
+ */
 export function tick_bounce_vertical(state, input) {
-	if (
-		state.ball_y == GAME_SETTINGS.ball_size ||
-		state.ball_y == GAME_DIMENSIONS[1] - GAME_SETTINGS.ball_size
-	)
-		state.ball_speed_y *= -1;
+  if (
+    state.ball_y == GAME_SETTINGS.ball_size ||
+    state.ball_y == GAME_DIMENSIONS[1] - GAME_SETTINGS.ball_size
+  )
+    state.ball_speed_y *= -1;
 }
 
 /**
-	* @param {GameState} state
-	* @returns {boolean}
-*/
-export function is_ball_on_left(state) {
-	return state.ball_x == GAME_SETTINGS.ball_size;
-}
-
-/**
-	* @param {GameState} state
-	* @returns {boolean}
-*/
-function is_ball_on_right(state) {
-	return state.ball_x == GAME_DIMENSIONS[0] - GAME_SETTINGS.ball_size;
-}
-
-/**
-	* @param {GameState} state
-	* @returns {boolean}
-*/
-export function is_ball_on_wall(state) {
-	return is_ball_on_left(state) || is_ball_on_right(state);
-}
-
-/**
-	* @param {ball_y} number
-	* @param {paddle_y} number
-	* @returns {boolean}
-*/
-export function is_ball_bouncing(ball_y, paddle_y) {
-	return (
-		ball_y - GAME_SETTINGS.ball_size < paddle_y + GAME_SETTINGS.paddle_height &&
-		ball_y + GAME_SETTINGS.ball_size > paddle_y - GAME_SETTINGS.paddle_height
-	);
-}
-
-/**
-	* @param {ball_y} number
-	* @param {paddle_y} number
-	* @returns {number}
-*/
+ * @param {number} ball_y
+ * @param {number} paddle_y
+ * @returns {number}
+ */
 export function ball_bounce_y_speed(ball_y, paddle_y) {
-	return (((ball_y - paddle_y) / GAME_SETTINGS.ball_y_speed_factor) | 0);
+  return ((ball_y - paddle_y) / GAME_SETTINGS.ball_y_speed_factor) | 0;
+}
+
+/**
+ * @param {GameState} state
+ * @param {number} playernum
+ * @returns {void}
+ * INFO: MODIFIES STATE
+ */
+function bounce_ball(state, playernum) {
+  state.ball_speed_x *= -1;
+  if (playernum == 1)
+    state.ball_speed_y = ball_bounce_y_speed(state.ball_y, state.p1_height);
+  else if (playernum == 2)
+    state.ball_speed_y = ball_bounce_y_speed(state.ball_y, state.p2_height);
+}
+
+/**
+ * @param {number} ball_y
+ * @param {number} paddle_y
+ * @returns {boolean}
+ */
+function is_ball_in_paddle_y_range(ball_y, paddle_y) {
+  return (
+    ball_y - GAME_SETTINGS.ball_size < paddle_y + GAME_SETTINGS.paddle_height &&
+    ball_y + GAME_SETTINGS.ball_size > paddle_y - GAME_SETTINGS.paddle_height
+  );
+}
+
+/**
+ * @param {GameState} state
+ * @param {vec2} input
+ * @returns {void}
+ * INFO: MODIFIES STATE
+ */
+export function tick_bounce_horizontal(state, input) {
+  if (
+    state.ball_speed_x < 0 &&
+    state.ball_x - GAME_SETTINGS.ball_size < GAME_SETTINGS.paddle_width &&
+    is_ball_in_paddle_y_range(state.ball_y, state.p1_height)
+  )
+    bounce_ball(state, 1);
+  else if (
+    state.ball_speed_x > 0 &&
+    state.ball_x + GAME_SETTINGS.ball_size >
+      GAME_DIMENSIONS[0] - GAME_SETTINGS.paddle_width &&
+    is_ball_in_paddle_y_range(state.ball_y, state.p2_height)
+  )
+    bounce_ball(state, 2);
+}
+
+/**
+ * @param {GameState} state
+ * @returns {boolean}
+ */
+function is_ball_on_left(state) {
+  return state.ball_x == GAME_SETTINGS.ball_size;
+}
+
+/**
+ * @param {GameState} state
+ * @returns {boolean}
+ */
+function is_ball_on_right(state) {
+  return state.ball_x == GAME_DIMENSIONS[0] - GAME_SETTINGS.ball_size;
+}
+
+/**
+ * @param {GameState} state
+ * @returns {number} - which player died, 0 if none
+ */
+export function detect_death(state) {
+  if (state.ball_speed_x < 0 && is_ball_on_left(state)) return 1;
+  if (state.ball_speed_x > 0 && is_ball_on_right(state)) return 2;
+  return 0;
 }
